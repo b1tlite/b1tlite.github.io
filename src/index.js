ed968489678cab9ae3a26b4fa76f5b6cd5c6fd6f
bohdan.sapozhnikov <bohdan.sapozhnikov@sensoramalab.com>
2022-05-10 12:34:03 +0300

    Export get nfts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import capital from './capital'
 import addDOMContent from './addDOMContent'
 import { init, loadNfts } from './chain'
+import { getNfts } from './web3Api'
 
-export { capital, addDOMContent, init, loadNfts }
+export { capital, addDOMContent, init, loadNfts, getNfts }
