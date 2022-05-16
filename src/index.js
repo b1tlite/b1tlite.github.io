6c0d25d99da426b2080f7774a7c43567011bd7a8
Bohdan Sapozhnikov <bohdan.sapozhnikov@sensoramalab.com>
2022-05-16 11:06:23 +0300

    Add three lib

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { init } from './old'
+import { runScene } from './three-lib'
 import { initialize } from './ui'
 import { buyNft, disableWeb3, enableWeb3, getNfts, getNFTsOwnedByUser, isUserOwnsSomeNfts } from './web3Api'
 
 const web3 = { isUserOwnsSomeNfts, connect: enableWeb3, disconnect: disableWeb3, getNfts, buyNft, getNFTsOwnedByUser }
-export { init, getNfts, initialize, web3 }
+const three = { runScene }
+export { init, getNfts, initialize, web3, three }
